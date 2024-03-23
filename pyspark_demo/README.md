682c90cfed8d9eb5efd93cff48dd4dae0c149ec3
Sandeep <sdp117@gmail.com>
2024-03-23 20:03:04 +0530

    Added SRC and DEST Path Created README.md for pyspark task

---
+++ pyspark_demo/README.md
@@ -0,0 +1,5 @@
+This a demo for PySpark
+<br>
+This application accepts a CSV file, 
+<br>
+Converts it to Parquet and writes output file to desired destination
--- pyspark_demo/get_all_variables.py
+++ pyspark_demo/get_all_variables.py
@@ -1,7 +1,7 @@
 import os
 
-os.environ['SRC_PATH']
-os.environ['DEST_PATH']
+os.environ['SRC_PATH'] = r"C:\Users\Sandeep\Desktop\data\cr.txt"
+os.environ['DEST_PATH'] = r"C:\Users\Sandeep\Desktop\data\cr.txt"
 
 src_path=os.environ['SRC_PATH']
 dest_path=os.environ['DEST_PATH']
